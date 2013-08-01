423263448f37e886220ce8bd1a7e214c8f640fa4
Ivan Leichtling <ivanlei@yelp.com>
2013-07-31 20:36:33 -0700

    Cleanups to rvm module now have pptest manifest properly building

--- manifests/pptest.pp
+++ manifests/pptest.pp
@@ -1,15 +1,15 @@
 require stdlib
 
 class { 'apt-cacher-ng::client':
-  server  => "${aptserver_ip}:3142",
-  autodetect => false,
-  stage => 'setup',
-  before => [Class['apt']]
+  server      => "${aptserver_ip}:3142",
+  autodetect  => false,
+  stage       => 'setup',
+  before      => [Class['apt']]
 }
 
 class { 'apt':
   always_apt_update => true,
-  stage => 'setup',
+  stage             => 'setup',
 }
 
 class fungo-bat {
