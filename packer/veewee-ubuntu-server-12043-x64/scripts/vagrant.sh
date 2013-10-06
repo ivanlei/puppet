9d4a9115566209c7c009a8900c49f8419b8ba6b0
Ivan Leichtling <ivanlei@yelp.com>
2013-10-06 12:44:56 -0700

    B Set vagrant boxes to require certs for SSH connections
    Uses augeas to dork /etc/ssh/sshd_config to forbid PasswordAuthentication

--- packer/veewee-ubuntu-server-12043-x64/scripts/vagrant.sh
+++ packer/veewee-ubuntu-server-12043-x64/scripts/vagrant.sh
@@ -3,6 +3,10 @@ date > /etc/vagrant_box_build_time
 # Setup sudo to allow no-password sudo for "sudo"
 usermod -a -G sudo vagrant
 
+# Setup SSH to not allow password auth
+apt-get install -y augeas-tools
+augtool -s set /files/etc/ssh/sshd_config/PasswordAuthentication no
+
 # Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
@@ -10,6 +14,5 @@ cd /home/vagrant/.ssh
 cp /tmp/id_rsa_vagrant.pub /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
-
 rm /tmp/id_rsa_vagrant.pub
 
